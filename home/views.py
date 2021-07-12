5966d73b09ddd694e605ca14bfc8fb1dde5b0f6c
Aku <akash.tvm@gmail.com>
2021-07-12 17:07:03 +0530

    testing

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Project, Profile_Pic
 
+
 def home(request):
     projects = Project.objects.order_by('-date')
     profilepic = Profile_Pic.objects.all()

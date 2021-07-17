64b997a4569c056f00b962ac97e5d4a69bbbff9b
Aku <akash.tvm@gmail.com>
2021-07-17 17:39:10 +0530

    render link, view in aboutus

--- about_us/views.py
+++ about_us/views.py
@@ -3,6 +3,6 @@ from .models import About
 
 def about_us(request):
     about = About.objects.all()
-    return render(request, 'about/about.html', {'about':about,})
+    return render(request, 'about_us/about.html', {'about':about,})
 
 # Create your views here.

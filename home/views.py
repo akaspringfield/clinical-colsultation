5558903042a55c7e52319c179438f0f492cae9f7
Aku <akash.tvm@gmail.com>
2021-07-28 17:47:33 +0530

    final changes views home blog

--- home/views.py
+++ home/views.py
@@ -5,4 +5,5 @@ from .models import Project, Profile_Pic
 def home(request):
     projects = Project.objects.order_by('-date')
     profilepic = Profile_Pic.objects.all()
+    return render(request, 'home/home.html',{'projects':projects,
     })

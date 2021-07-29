7abcfdb2e9c69cedba2e2bde44fb939a68e03f21
Aku <akash.tvm@gmail.com>
2021-07-29 15:49:44 +0530

    final commit blog with changes in char

--- home/models.py
+++ home/models.py
@@ -5,8 +5,8 @@ class Profile_Pic(models.Model):
     img = models.ImageField(upload_to='home/images')
 
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
     image = models.ImageField(upload_to='home/images',)
     url = models.URLField(blank=True)
     date = models.DateTimeField(default=timezone.now)

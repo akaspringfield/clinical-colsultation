989d509cb75eb1e21165d29f0d9e878d0714a596
Aku <akash.tvm@gmail.com>
2021-07-29 14:44:43 +0530

    final commit blog

--- home/views.py
+++ home/views.py
@@ -6,4 +6,5 @@ def home(request):
     projects = Project.objects.order_by('-date')
     profilepic = Profile_Pic.objects.all()
     return render(request, 'home/home.html',{'projects':projects,
+    'profilepic':profilepic,
     })
